516f81fda41369fd1d0352af3ab34862615abdf6
SPereda2005 <spereda2024@alu.uct.cl>
2024-06-03 09:33:35 -0400

    cambio

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ import sqlite3 as SQL
 
 conexion=SQL.connect("db_personas.db")
 hola = conexion.cursor()
-hola.execute("SELECT rut, FROM personas")
+hola.execute("SELECT rut FROM personas")
 como = hola.fetchall()
 for k in hola:
     print(k)
